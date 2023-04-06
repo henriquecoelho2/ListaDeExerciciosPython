2fd49f4c1e4109b0c208787e840a230818da1983
Henrique Coelho Vargas <henriquecoelho.vargas@gmail.com>
2023-04-06 14:54:29 -0300

    terminei o exercicio 5

--- Exercicio5.py
+++ Exercicio5.py
@@ -1,11 +1,11 @@
 VetorDe20numeros= [2, 5, 6, 3, 4, 6, 8, 4, 67, 4657, 343, 4312, 89 ,2341 , 678, 32 ,7, 61 ,87, 2356]
-
+print (VetorDe20numeros)
 Pares=[]
 Impares=[]
 for i in VetorDe20numeros:
     if i%2 == 0:
-        Pares = i
+        Pares.append(i)
     else:
-        Impares = i
-    print ('os valores pares são ' + str(Pares))
-    print('os valores impares são ' + str(Impares))
+        Impares.append(i)
+print ('os valores pares são ' + str(Pares))
+print('os valores impares são ' + str(Impares))
