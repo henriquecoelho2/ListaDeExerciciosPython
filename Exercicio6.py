9bfd2dcd540e7a55119438f21db34bc8533580d4
Henrique Coelho Vargas <henriquecoelho.vargas@gmail.com>
2023-04-13 10:49:55 -0300

    terminei o exercício 7

--- Exercicio6.py
+++ Exercicio6.py
@@ -1,4 +1,5 @@
-alunosNotas = [['Caio', [1, 5, 8 ,3]], ['Maykon', [7, 7, 1, 7]], ['Henrique', [7, 5, 8, 3]], ['Davi', [1, 1 ,1, 1]], ['Barbosa', [4, 7, 3, 7]], ['Miguel', [5, 2, 8, 4]], ['Leticia', [6, 6, 6, 1]], ['Natalia', [10 ,10, 1 ,1]], ['Farias', [2, 3, 10, 1]], ['Ian', [2, 4, 5, 3]]]
+alunosNotas = [['Caio', [1, 8 ,5, 3]], ['Maykon', [7, 7, 1, 7]], ['Henrique', [7, 5, 8, 3]], ['Davi', [1, 1 ,1, 1]], ['Barbosa', [4, 7, 3, 7]], ['Miguel', [5, 2, 8, 4]], ['Leticia', [6, 6, 6, 1]], ['Natalia', [10 ,10, 1 ,1]], ['Farias', [2, 3, 10, 1]], ['Ian', [2, 4, 5, 3]]]
+numeroDeAlunos = 0
 
 def calcular_media_notas(aluno):
     notas = aluno[1]
@@ -8,5 +9,8 @@ def calcular_media_notas(aluno):
 for aluno in alunosNotas:
     nome = aluno[0]
     media = calcular_media_notas(aluno)
-    print(f'O aluno {nome} teve média {media:.2f}')
+    if media >= 5:
+        numeroDeAlunos = numeroDeAlunos + 1
+print(numeroDeAlunos)
+
 
